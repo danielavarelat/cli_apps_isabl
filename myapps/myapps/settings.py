3edb7eb76b34d59e0ab3f1d925ac1487755539ab
danielavarelat <danielavarelatabares@gmail.com>
2019-10-07 12:38:09 -0400

    config file gridss

--- myapps/myapps/settings.py
+++ myapps/myapps/settings.py
@@ -1,12 +1,7 @@
 from myapps import apps
 from myapps import constants
 
-apps.BwaMemGRCh38().patch_application_settings(
-        **{
-            "reference": constants.GRCH38_FASTA,
-            "cores": {"WG": 32, "WE": 32, "TD": 4},
-        }
-)
+
 apps.Gridss().patch_application_settings(
         **{
             "config": constants.GRIDSS_CONFIG,
